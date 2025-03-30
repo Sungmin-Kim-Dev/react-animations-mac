f803cfa00bd59d0457ec76d23937b0d67a15b34d
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-03-30 23:51:39 +0900

    variant-one

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import {motion} from "motion/react";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 1rem;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const myVariants = {
+  start: {
+    scale: 0,
+  },
+  end: {
+    scale: 1,
+    rotateZ: 360,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+const App = () => {
+  return (
+    <Wrapper>
+      <Box variants={myVariants} initial="start" animate="end" />
+    </Wrapper>
+  );
+};
+
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,4 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const darkTheme: DefaultTheme = {
+};
