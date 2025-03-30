85e6bd49d902d267e7a5c0b33a0cc32c28711a4f
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-03-31 00:20:42 +0900

    variant-two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {motion} from "motion/react";
+import {motion, Variants} from "motion/react";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -11,29 +11,61 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 1rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 3rem;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVariants = {
+const Circle = styled(motion.div)`
+  background-color: white;
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants: Variants = {
   start: {
-    scale: 0,
+    opacity: 0,
+    scale: 0.5,
   },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
     transition: {
       type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
       delay: 0.5,
+      delayChildren: 0.6,
+      staggerChildren: 0.2,
     },
+  },
+};
+
+const circleVariants: Variants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 const App = () => {
   return (
     <Wrapper>
-      <Box variants={myVariants} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 };
