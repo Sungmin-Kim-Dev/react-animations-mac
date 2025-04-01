cf362c2ddc09bfeb66eb763dd8a777fc99248339
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-01 23:32:15 +0900

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
+  /* flex-wrap: wrap; */
+  gap: 1rem;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -14,36 +17,61 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 100px;
   background-color: rgba(255, 255, 255, 1);
-  /* place-self: center; */
-  border-radius: 3rem;
+  align-content: center;
+  text-align: center;
+  border-radius: 2rem;
+  font-size: 3rem;
   position: absolute;
-  top: 50px;
+  top: 5rem;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-const boxVariants = {
-  initial: {
+const boxVariant = {
+  entry: (back: boolean) => ({
+    x: back ? -300 : 300,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 0.3,
+    },
   },
-  exit: {
+  exit: (back: boolean) => ({
+    x: back ? 300 : -300,
     opacity: 0,
-    y: 20,
-  },
+    scale: 0,
+    rotateX: 180,
+    transition: {
+      duration: 0.3,
+    },
+  }),
 };
 
 const App = () => {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 10 : prev - 1));
+  };
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 1 : prev + 1));
+  };
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-      <AnimatePresence>{showing && <Box variants={boxVariants} initial="initial" animate="visible" exit="exit" />}</AnimatePresence>
+      <AnimatePresence mode="sync" custom={back}>
+        <Box custom={back} variants={boxVariant} initial="entry" animate="center" exit="exit" key={visible}>
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <div style={{display: "flex", gap: "2rem"}}>
+        <button onClick={prevPlease}>Previous</button>
+        <button onClick={nextPlease}>Next</button>
+      </div>
     </Wrapper>
   );
 };
